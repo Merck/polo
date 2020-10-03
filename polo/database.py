140cb43a7ae863f1ec4a979e945f0696b8a5e555
clesburg <charles.lesburg@merck.com>
2020-10-03 08:45:09 -0400

    Remove console logging from HtmlSanitizer Added option to disable sparklines for faster loading Added pre-ping to database engine to mitigate disconnections

--- polo/database.py
+++ polo/database.py
@@ -10,11 +10,11 @@ import urllib.parse
 from sqlalchemy import create_engine
 from polo import app
 
-polo_engine = create_engine('mysql://%s' % app.config['POLO_CONN'])
+polo_engine = create_engine('mysql://%s' % app.config['POLO_CONN'], pool_pre_ping=True)
 
 rm_engines = {}
 for i in app.config['RM_CONN'].keys():
     rm_conn = app.config['RM_CONN'][i]
     params = urllib.parse.quote_plus(rm_conn)
-    engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+    engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params, pool_pre_ping=True)
     rm_engines[i] = engine
